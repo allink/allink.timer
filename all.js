375c7189a09f9d153f01c80e56f2be50ba4450ce
Silvan Spross <silvan.spross@gmail.com>
2010-09-06 14:40:09 +0200

    speed improvement added loader fixed projects dropdown bug

--- all.js
+++ all.js
@@ -1,99 +1,117 @@
 $(document).ready(function() {
     
     // reminder all 30min
-    $(this).everyTime('1800s',function(i) {
-        window.fluid.showGrowlNotification({
-           'title': $('#company').text(),
-           'description': "don't forget to track your time",
-        });
-    });
+    // $(this).everyTime('1800s',function(i) {
+    //     window.fluid.showGrowlNotification({
+    //        'title': COMPANY.text(),
+    //        'description': "don't forget to track your time",
+    //     });
+    // });
     
     var COOKIE_BASE_URL = 'base_url';
     var COOKIE_API_TOKEN = 'api_token';
     var COOKIE_PROJECTS = 'projects_cache';
     var COOKIE_OPTIONS = { path: '/', expires: 360 };
     
+    var BASE_URL = $('#baseUrl');
+    var API_TOKEN = $('#apiToken');
+    var TIME_LINK = $('#time_link');
+    var PROJECTS = $('#projects');
+    var SETTINGS = $('#settings');
+    var SETTINGS_LINK = $('#settings_link');
+    var HISTORY = $('#history');
+    var HISTORY_LINK = $('#history_link');
+    var TIME = $('#time');
+    var COMPANY = $('#company');
+    var COMPANIES = $('#companies');
+    var USER_ID = $('#userId');
+    var HOURS = $('#hours');
+    var DESC = $('#desc');
+    var LOADER = $('#loader');
+    var USER = $('#user');
+    var HISTORY_LIST = $('#history_list');
+    var ADD_BUTTON = $('#addButton');
+    
     function startUp() {
-        $('#baseUrl').attr('value', $.cookie(COOKIE_BASE_URL));
-        $('#apiToken').attr('value', $.cookie(COOKIE_API_TOKEN));
+        BASE_URL.attr('value', $.cookie(COOKIE_BASE_URL));
+        API_TOKEN.attr('value', $.cookie(COOKIE_API_TOKEN));
         checkSettings();
         refreshCompanies(baseUrl, apiToken);
     }
     
     function checkSettings() {   
-        baseUrl = $('#baseUrl').attr('value');
-        apiToken = $('#apiToken').attr('value');
+        baseUrl = BASE_URL.attr('value');
+        apiToken = API_TOKEN.attr('value');
         
         if (baseUrl != '' && apiToken != '') {
             refreshAccount(baseUrl, apiToken);
         }
     }
     
-    $('#baseUrl').change(function() {
-        $.cookie(COOKIE_BASE_URL, $('#baseUrl').attr('value'), COOKIE_OPTIONS);
+    BASE_URL.change(function() {
+        $.cookie(COOKIE_BASE_URL, BASE_URL.attr('value'), COOKIE_OPTIONS);
         checkSettings();
     });
     
-    $('#apiToken').change(function() {
-        $.cookie(COOKIE_API_TOKEN, $('#apiToken').attr('value'), COOKIE_OPTIONS);
+    API_TOKEN.change(function() {
+        $.cookie(COOKIE_API_TOKEN, API_TOKEN.attr('value'), COOKIE_OPTIONS);
         checkSettings();
     });
 
-    $('#time_link').click(function() {
-        $('#projects').remove();
+    TIME_LINK.click(function() {
+        PROJECTS.hide();
         
-        $('#settings').hide();
-        $('#settings_link').removeClass('current');
-        $('#history').hide();
-        $('#history_link').removeClass('current');
+        SETTINGS.hide();
+        SETTINGS_LINK.removeClass('current');
+        HISTORY.hide();
+        HISTORY_LINK.removeClass('current');
         
-        $('#time_link').addClass('current');
-        $('#time').show();
+        TIME_LINK.addClass('current');
+        TIME.show();
         
         refreshAccount(baseUrl, apiToken);
         refreshCompanies(baseUrl, apiToken);
     });
     
-    $('#history_link').click(function() {
-        $('#time').hide();
-        $('#time_link').removeClass('current');
-        $('#settings').hide();
-        $('#settings_link').removeClass('current');
+    HISTORY_LINK.click(function() {
+        TIME.hide();
+        TIME_LINK.removeClass('current');
+        SETTINGS.hide();
+        SETTINGS_LINK.removeClass('current');
         
-        $('#history_link').addClass('current');
-        $('#history').show();
-        
-        console.log($.cookie(COOKIE_PROJECTS));
+        HISTORY_LINK.addClass('current');
+        HISTORY.show();
         
         refreshAccount(baseUrl, apiToken);
         refreshHistory(baseUrl, apiToken);
     });
     
-    $('#settings_link').click(function() {
-        $('#time').hide();
-        $('#time_link').removeClass('current');
-        $('#history').hide();
-        $('#history_link').removeClass('current');
+    SETTINGS_LINK.click(function() {
+        TIME.hide();
+        TIME_LINK.removeClass('current');
+        HISTORY.hide();
+        HISTORY_LINK.removeClass('current');
         
-        $('#settings_link').addClass('current');
-        $('#settings').show();
+        SETTINGS_LINK.addClass('current');
+        SETTINGS.show();
     });
     
-    $('#companies').change(function() {
+    COMPANIES.change(function() {
         var companyId = parseInt($(this).attr('value'));
         if (companyId > 0) {
-            $('#projects').show();
             refreshProjects(baseUrl, apiToken, companyId);
         }
     });
     
-    $('#addButton').click(function() {
-        var userId = parseInt($('#userId').text());
-        var companyId = parseInt($('#companies').attr('value'));
-        var projectId = parseInt($('#projects').attr('value'));
-        var projectName = $('option:selected','#projects').text();
-        var hours = $('#hours').attr('value');
-        var desc = $('#desc').attr('value');
+    ADD_BUTTON.click(function() {
+        LOADER.show();
+        
+        var userId = parseInt(USER_ID.text());
+        var companyId = parseInt(COMPANIES.attr('value'));
+        var projectId = parseInt(PROJECTS.attr('value'));
+        var projectName = $('option:selected',PROJECTS).text();
+        var hours = HOURS.attr('value');
+        var desc = DESC.attr('value');
         var today = new Date();
         var date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
         
@@ -110,12 +128,13 @@ $(document).ready(function() {
                 complete: function(request, status) {
                     if (parseInt(request.status) == 201) {
                         window.fluid.showGrowlNotification({
-                           'title': $('#company').text(),
+                           'title': COMPANY.text(),
                            'description': "tracked " + hours + " hours on " + projectName,
                         });
-                        $('#desc').attr('value','');
-                        $('#hours').attr('value','');
-                    }
+                        DESC.attr('value','');
+                        HOURS.attr('value','');
+                    };
+                    LOADER.hide();
                 },
             });
         }
@@ -125,6 +144,7 @@ $(document).ready(function() {
     
     function refreshAccount(baseUrl, apiToken) {
         
+        LOADER.show();
         $.ajax({
             type: 'GET',
             url: baseUrl + '/account.xml',
@@ -135,7 +155,7 @@ $(document).ready(function() {
             },
             success: function(msg) {
                 $(msg).find('account:first').each(function() {
-                    $('#company').text($(this).find('name').text());
+                    COMPANY.text($(this).find('name:first').text());
                 });
             }
         });
@@ -150,8 +170,9 @@ $(document).ready(function() {
             },
             success: function(msg) {
                 $(msg).find('person:first').each(function() {
-                    $('#user').text($(this).find('first-name').text() + ' ' + $(this).find('last-name').text());
-                    $('#userId').text($(this).find('id').text());
+                    USER.text($(this).find('first-name').text() + ' ' + $(this).find('last-name').text());
+                    USER_ID.text($(this).find('id').text());
+                    LOADER.hide();
                 });
             }
         });
@@ -160,8 +181,10 @@ $(document).ready(function() {
     
     function refreshCompanies(baseUrl, apiToken) {
         
-        $('#companies').empty();
-        $('#companies').append('<option value="0">please select...</option>');
+        LOADER.show();
+        COMPANIES.hide();
+        COMPANIES.empty();
+        COMPANIES.append('<option value="0">please select...</option>');
         $.ajax({
             type: 'GET',
             url: baseUrl + '/companies.xml',
@@ -179,8 +202,10 @@ $(document).ready(function() {
                 $(sorted).each(function() {
                     var id = $(this).find('id').text();
                     var name = $(this).find('name').text();
-                    $('<option value="' + id + '"></option>').html(name).appendTo('#companies');
+                    $('<option value="' + id + '"></option>').html(name).appendTo(COMPANIES);
                 });
+                COMPANIES.show();
+                LOADER.hide();
             }
         });
         
@@ -188,10 +213,12 @@ $(document).ready(function() {
     
     function refreshProjects(baseUrl, apiToken, companyId) {
         
-        var projects = new Array();
+        //var projects = new Array();
         
-        $('#projects').empty();
-        $('#projects').append('<option value="0">please select...</option>');
+        LOADER.show();
+        PROJECTS.hide();
+        PROJECTS.empty();
+        PROJECTS.append('<option value="0">please select...</option>');
         $.ajax({
             type: 'GET',
             url: baseUrl + '/projects.xml',
@@ -212,24 +239,28 @@ $(document).ready(function() {
                 $(sorted).each(function() {
                     var id = $(this).find('id:first').text();
                     var name = $(this).find('name:first').text();
-                    projects[id] = name;
-                    $('<option value="' + id + '"></option>').html(name).appendTo('#projects');
+                    //projects[id] = name;
+                    $('<option value="' + id + '"></option>').html(name).appendTo(PROJECTS);
                 });
-                $.cookie(COOKIE_PROJECTS, projects, COOKIE_OPTIONS);
+                //$.cookie(COOKIE_PROJECTS, projects, COOKIE_OPTIONS);
+                PROJECTS.show();
+                LOADER.hide();
             }
         }); 
         
     }
     
     function refreshHistory(baseUrl, apiToken) {
-        var userId = parseInt($('#userId').text());
+        LOADER.show();
+        
+        var userId = parseInt(USER_ID.text());
         var today = new Date();
         var year = today.getFullYear();
         var month = (today.getMonth() + 1);
-        var day = today.getDate() - 1;
+        var day = today.getDate();
         var date = year + (month < 10 ? '0' : '') + month + (day < 10 ? '0' : '') + day;
         
-        $('#history_list').empty();
+        HISTORY_LIST.empty();
         $.ajax({
             type: 'GET',
             url: baseUrl + '/time_entries/report.xml',
@@ -240,13 +271,17 @@ $(document).ready(function() {
                 xhr.setRequestHeader('Accept','application/xml');
             },
             success: function(msg) {
+                var total = 0.0;
                 $(msg).find('time-entry').each(function() {
                     var hours = $(this).find('hours').text();
                     var projectId = $(this).find('project-id').text();
                     var projectName = getProjectName(baseUrl, apiToken, projectId);
                     var desc = $(this).find('description').text();
-                    $('<li></li>').html(projectName + ' - ' + hours + 'h - ' + desc).appendTo('#history_list');
+                    $('<li></li>').html(projectName + ' - ' + hours + 'h - ' + desc).appendTo(HISTORY_LIST);
+                    total = total + parseFloat(hours);
                 });
+                $('<li></li>').html('<strong>Total ' + total + 'h</strong>').appendTo(HISTORY_LIST);
+                LOADER.hide();
             }
         });
         
@@ -255,6 +290,7 @@ $(document).ready(function() {
     function getProjectName(baseUrl, apiToken, projectId) {
         var projectName = '';
         
+        LOADER.show();
         $.ajax({
             type: 'GET',
             url: baseUrl + '/projects/' + projectId + '.xml',
@@ -265,7 +301,8 @@ $(document).ready(function() {
                 xhr.setRequestHeader('Accept','application/xml');
             },
             success: function(msg) {
-                projectName = $(msg).find('name').text();
+                projectName = $(msg).find('name:first').text() + " (" + $(msg).find('name:last').text() + ")";
+                LOADER.hide();
             }
         });
         
