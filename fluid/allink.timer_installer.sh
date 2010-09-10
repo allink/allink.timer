1ea0dd873229f9ef033cb1dc8ef21490dd855927
Silvan Spross <silvan.spross@gmail.com>
2010-09-10 11:01:43 +0200

    updated plist and installer script

---
+++ fluid/allink.timer_installer.sh
@@ -0,0 +1,2 @@
+cp -fR /Volumes/stuff/software/allink.timer/allink.timer.app /Applications/
+cp -f /Volumes/stuff/software/allink.timer/com.fluidapp.FluidInstance.allink.timer.plist ~/Library/Preferences/
